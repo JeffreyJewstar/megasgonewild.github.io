d5ee2b3fb84a53f1af7de6c0483cc09e8a63bc79
JeffreyJewstar <anpadhshivansh@gmail.com>
2022-01-27 21:12:44 +0530

    sex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { AboutComponent } from './sharepage/pages/about/about.component';
+import { HomeComponent } from './sharepage/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
